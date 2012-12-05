d8387e2a179ef573f64675684ed54b6299f7d266
josisusan <josisusan@gmail.com>
2012-12-05 21:42:33 +0545

    set up of enw rail app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieReviewer::Application.config.secret_token = '9258d46184b54a9c706d914f265155d693101ae5064eec83c2c87e4917b5b98cc53db1b1c3c6fdf2de53e1980028218c0ce618a645a7536d16dffdfeb839783f'
